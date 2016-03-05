87aae864469d788cb71792078915cd219e6331cf
Jesse Qin <itzjesse@gmail.com>
2016-03-04 20:52:08 -0800

    gewgwa

--- public/js/main.js
+++ public/js/main.js
@@ -3,3 +3,12 @@ $(document).ready(function () {
     swapText($('.var-1'), 'var-in', $(this).text().trim().toLowerCase());
   });
 });
+$(window).load(function () {
+  // Handle variable height fixed header padding
+  function adjustVariableHeader () {
+    $('.narrative').css('padding-bottom', $('.narrative').outerHeight(true) * 0.33);
+    $('.under-narrative-content').css('padding-top', $('.narrative').outerHeight(true));
+  }
+  adjustVariableHeader();
+  $(this).resize(adjustVariableHeader);
+});
