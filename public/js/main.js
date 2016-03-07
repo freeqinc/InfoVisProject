5b4480f5c473d61e1a0758325b63cd0ca68df698
Jesse Qin <itzjesse@gmail.com>
2016-03-07 11:16:24 -0800

    asdf

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,19 @@
-$(document).ready(function () {
-  $('.input-people-choice').click(function () {
-    swapText($('.var-1'), 'var-in', $(this).text().trim().toLowerCase());
-  });
-});
+/**
+ * Data
+ */
+var state = {};
+state.people = 0; // how many people
+state.duration = 4; // weeks of one less shower per week
+state.textPeople = ''; // narrative text for # people
+state.textDuration = ''; // narrative text for duration
+state.textComparison = ''; // narrative text for comparison
+state.log = function () { // for debugging
+  console.log(this);
+};
+
+/**
+ * Handling some responsive things
+ */
 $(window).load(function () {
   // Handle variable height fixed header padding
   function adjustVariableHeader () {
@@ -11,4 +22,20 @@ $(window).load(function () {
   }
   adjustVariableHeader();
   $(this).resize(adjustVariableHeader);
+});
+
+/**
+ * Main driver
+ */
+$(document).ready(function () {
+  $('.input-people-choice').click(function () {
+    var textIn = $(this).data('narrative');
+    var numIn = $(this).data('num-people');
+    swapText($('.var-1'), 'var-in', textIn);
+    $('.input-people-choice').removeClass('input-people-choice-selected');
+    $(this).addClass('input-people-choice-selected');
+    state.people = numIn;
+    state.textPeople = textIn;
+    state.log();
+  });
 });
