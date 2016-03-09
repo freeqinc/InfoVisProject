3bb744b5c5fc29e7f3d66e7084cce85ae1ed0de3
Jesse Qin <itzjesse@gmail.com>
2016-03-08 23:31:16 -0800

    Agewa

--- app.js
+++ app.js
@@ -1,34 +1,30 @@
-(function() {
-    'use strict';
+var express = require('express');
+var app = express();
+var exphbs = require('express-handlebars');
+var path = require('path');
+var sassMiddleware = require('node-sass-middleware');
 
-    let express = require('express');
-    let app = express();
-    let exphbs = require('express-handlebars');
-    let path = require('path');
-    let sassMiddleware = require('node-sass-middleware');
+var index = require('./controllers/index');
 
-    let index = require('./controllers/index');
+app.use(sassMiddleware({
+    /* Options */
+    src: path.join(__dirname, 'public/css'),
+    dest: path.join(__dirname, 'public/css'),
+    debug: true,
+    outputStyle: 'compressed',
+    prefix: '/css'
+}));
 
-    app.use(sassMiddleware({
-        /* Options */
-        src: path.join(__dirname, 'public/css'),
-        dest: path.join(__dirname, 'public/css'),
-        debug: true,
-        outputStyle: 'compressed',
-        prefix: '/css'
-    }));
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'));
+app.use('/css', express.static(path.join(__dirname, 'public/css')));
+app.use('/fonts', express.static(path.join(__dirname, 'public/fonts')));
+app.use('/js', express.static(path.join(__dirname, 'public/js')));
+app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
+app.use(express.static(path.join(__dirname, 'public')));
 
-    app.engine('handlebars', exphbs({
-        defaultLayout: 'main'
-    }));
-    app.set('view engine', 'handlebars');
-    app.set('views', path.join(__dirname, 'views'));
-    app.use('/css', express.static(path.join(__dirname, 'public/css')));
-    app.use('/fonts', express.static(path.join(__dirname, 'public/fonts')));
-    app.use('/js', express.static(path.join(__dirname, 'public/js')));
-    app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
-    app.use(express.static(path.join(__dirname, 'public')));
-
-    app.get('/', index.index);
-    app.listen(3000);
-}());
+app.get('/', index.index);
+app.listen(3000);
